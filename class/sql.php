32bf0d7eecf11abf17c08e8d882f30fb24cd3e10
Mariana Ferreti <marianaferretigoncalves@gmail.com.br>
2026-01-05 10:15:28 -0300

    aprendendo DAO (separando o banco de dados da lógica

---
+++ class/sql.php
@@ -0,0 +1,41 @@
+<?php 
+
+class Sql extends PDO {
+
+	private $conn;
+
+	public function __construct() {
+		parent::__construct(
+			"mysql:dbname=dbphp7;host=localhost",
+			"root",
+			"",
+			[
+				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+			]
+		);
+	}
+
+	private function setParams($statement, $parameters = []) {
+		foreach ($parameters as $key => $value) {
+			$this->setParam($key, $value);
+		}
+	}
+
+	private function setParam($statement, $key, $value) {
+		$statement->bindValue($key, $value);
+	}
+
+	public function queryParams($rawQuery, $params = []) {
+		$stmt = $this->prepare($rawQuery);
+		$this->setParams($stmt, $params);
+		$stmt->execute();
+		return $stmt;
+	}
+
+	public function select($rawQuery, $params = []): array {
+		return $this->queryParams($rawQuery, $params)
+		            ->fetchAll(PDO::FETCH_ASSOC);
+	}
+}
+
+ ?>

d3f2b558bce4442bca3304e405f5b46ee6558fd0
Mariana Ferreti <marianaferretigoncalves@gmail.com.br>
2026-01-05 11:15:56 -0300

    métodos de lista

--- index.php
+++ index.php
@@ -3,15 +3,25 @@
 require_once("config.php");
 
 // $sql = new Sql();
-
 // $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 // echo json_encode($usuarios);
 
+//Carrega um usuário
+// $usuario = new Usuarios();
+// $usuario->loadById(3);
+// echo $usuario;
+
+//Carrega uma lista de usuários
+// $lista = Usuarios::getList();
+// echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo login
+// $search = Usuarios::search("root");
+// echo json_encode($search);
+
+//Carrega um usuário usando o login e a senha
 $usuario = new Usuarios();
-
-$usuario->loadById(3);
-
+$usuario->login("root", "5678");
 echo $usuario;
 
  ?>
